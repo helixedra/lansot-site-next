406b57c7d5c442f9dd05b8ee134b92c6ae235c06
helixedra <greencastle08@gmail.com>
2025-02-16 23:13:00 +0100

    _some_upd

--- app/[locale]/about/page.js
+++ app/[locale]/about/page.js
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import classes from './page.module.css';
-import page from '@/app/data/pages.json';
-import PageTitle from '@/components/shared/PageTitle';
+import Image from "next/image";
+import classes from "./page.module.css";
+import page from "@/app/data/pages.json";
+import PageTitle from "@/components/shared/PageTitle";
 
 export default async function AboutPage({ params }) {
   const { locale } = await params;
@@ -9,44 +9,22 @@ export default async function AboutPage({ params }) {
 
   return (
     <div className="max-w-[1600px] mx-auto mb-12 px-6 lg:px-12">
-      <div
-        data-aos="fade-up"
-        data-aos-duration="1000"
-        className="PageHeader py-8 lg:py-24">
-        <div className="PageHeader__subheader text-zinc-500 font-semibold mb-8">
-          {data.title}
-        </div>
+      <div data-aos="fade-up" data-aos-duration="1000" className="PageHeader py-8 lg:py-24 lg:mb-12">
+        <div className="PageHeader__subheader text-zinc-500 font-semibold mb-8">{data.title}</div>
         <h1 className="PageHeader__title max-w-[920px]">{data.subtitle}</h1>
       </div>
 
-      <div className="gap-8 flex lg:flex-row flex-col-reverse">
-        <div
-          data-aos="fade-up"
-          data-aos-duration="1000"
-          dangerouslySetInnerHTML={{ __html: data.content_p1 }}
-          className="lg:w-4/12 lg:pr-16 mt-[-1rem] sm:w-full lg:text-xl lg:leading-9"></div>
-        <div
-          data-aos="fade-up"
-          data-aos-duration="1000"
-          className="w-full sm:w-full lg:w-8/12">
-          <Image
-            src={'/images/home_slider_img_0.jpg'}
-            width={1600}
-            height={830}
-            alt="Idea SL"
-            style={{ objectFit: 'cover' }}
-          />
+      <div className="gap-8 flex lg:flex-row flex-col-reverse border-t border-black pb-32 pt-8">
+        <div data-aos="fade-up" data-aos-duration="1000" dangerouslySetInnerHTML={{ __html: data.content_p1 }} className="lg:w-4/12 lg:pr-16 mt-[-1rem] sm:w-full lg:text-xl lg:leading-9"></div>
+        <div data-aos="fade-up" data-aos-duration="1000" className="w-full sm:w-full lg:w-8/12">
+          <Image src={"/images/home_slider_img_0.jpg"} width={1600} height={830} alt="Idea SL" style={{ objectFit: "cover" }} />
         </div>
       </div>
 
       <div className="mt-8 lg:mt-24">
         <ul>
           {data.features.map((feature, index) => (
-            <li
-              key={index}
-              data-aos="fade-up-left"
-              data-aos-duration="1000"
-              className="flex flex-col lg:flex-row gap-4 border-t border-black pb-32 pt-8">
+            <li key={index} data-aos="fade-up-left" data-aos-duration="1000" className="flex flex-col lg:flex-row gap-4 border-t border-black pb-32 pt-8">
               <div className="lg:w-1/2">
                 <h2 className="uppercase max-w-[500px]">{feature.title}</h2>
               </div>
@@ -54,6 +32,11 @@ export default async function AboutPage({ params }) {
             </li>
           ))}
         </ul>
+      </div>
+
+      <div className="mt-8 lg:mt-24 relative mb-32">
+        <Image data-aos="fade-up" data-aos-duration="1000" src={"/images/products/delta-t2/delta_t2_promo_img_4.jpg"} width={2000} height={1500} style={{ objectFit: "cover" }} alt="delta 2" />
+        <div data-aos="fade-up" data-aos-duration="1000" dangerouslySetInnerHTML={{ __html: data.content_p2 }} className="lg:absolute left-0 text-lg bottom-0 lg:text-white mt-8 lg:p-24 sm:w-full lg:text-2xl lg:leading-9 "></div>
       </div>
     </div>
   );
--- app/[locale]/delivery/page.js
+++ app/[locale]/delivery/page.js
@@ -1,16 +1,53 @@
 import pages from "@/app/data/pages.json";
 import classes from "./page.module.css";
-import PageTitle from "@/components/shared/PageTitle";
 
 export default async function DeliveryPage({ params }) {
   const { locale } = await params;
   const data = await pages.delivery[locale];
 
   return (
-    <div>
-      <PageTitle title={data.title} image={"sc-3_12_1s.jpg"} alt={""} />
-      {/* <h1 className="page_heading">{data.title}</h1> */}
-      <div className="container">
+    <div className="max-w-[1600px] mx-auto mb-12 px-6 lg:px-12">
+      <div data-aos="fade-up" data-aos-duration="1000" className="PageHeader py-8 lg:py-24">
+        <div className="PageHeader__subheader text-zinc-500 font-semibold mb-8">{data.title}</div>
+        <h1 className="PageHeader__title max-w-[920px]">{data.subtitle}</h1>
+      </div>
+
+      <div className="mt-8 lg:mt-24">
+        <ul>
+          <li data-aos="zoom-out" data-aos-duration="1000" className="flex flex-col lg:flex-row gap-4 border-t border-black pb-32 pt-8">
+            <div className="lg:w-1/2">
+              <h2 className="uppercase max-w-[500px] mt-[-1rem] mb-16">{data.info.payment.title}</h2>
+            </div>
+            <div className="lg:w-1/2">
+              <div>
+                {data.info.payment.description.map((item, index) => (
+                  <>
+                    <div data-aos="fade-up-left" data-aos-duration="1000" className="mb-16 lg:mb-32 text-xl" key={index} dangerouslySetInnerHTML={{ __html: item }}></div>
+                    {/* <div className="h-1 bg-zinc-800 w-16 mb-8"></div> */}
+                  </>
+                ))}
+              </div>
+            </div>
+          </li>
+          <li data-aos="zoom-out" data-aos-duration="1000" className="flex flex-col lg:flex-row gap-4 border-t border-black pb-32 pt-8">
+            <div className="lg:w-1/2">
+              <h2 className="uppercase max-w-[500px] mt-[-1rem] mb-16">{data.info.delivery.title}</h2>
+            </div>
+            <div className="lg:w-1/2">
+              <div>
+                {data.info.delivery.description.map((item, index) => (
+                  <>
+                    <div data-aos="fade-up-left" data-aos-duration="1000" className="mb-16 lg:mb-32 text-xl" key={index} dangerouslySetInnerHTML={{ __html: item }}></div>
+                    {/* <div className="h-1 bg-zinc-800 w-16 mb-8"></div> */}
+                  </>
+                ))}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      {/* <div className="container">
         <div className={classes.delivery_text}>
           <h2>{data.info.payment.title}</h2>
           <ul className={classes.info_bullets}>
@@ -27,7 +64,7 @@ export default async function DeliveryPage({ params }) {
             ))}
           </ul>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
