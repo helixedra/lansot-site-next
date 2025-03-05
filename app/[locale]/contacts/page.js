9f13cfea9f027d76eac619bb2a85e104af62997d
helixedra <greencastle08@gmail.com>
2025-03-06 00:27:03 +0100

    upd5

--- app/[locale]/contacts/page.js
+++ app/[locale]/contacts/page.js
@@ -108,7 +108,7 @@ export default async function ContactsPage({ params }) {
           ))}
         </div>
       </div>
-      <ContactSection />
+      <ContactSection locale={locale} />
     </>
   );
 }
