2c0415f8865774eeabd3727a006a3cc20bcb6bac
helixedra <greencastle08@gmail.com>
2025-03-08 14:21:17 +0100

    some upd

--- middleware.js
+++ middleware.js
@@ -14,33 +14,27 @@ export function middleware(request) {
     pathname.startsWith("/sitemap.xml") ||
     pathname.startsWith("/images")
   ) {
-    return NextResponse.next();
+    return NextResponse.next("/public" + pathname);
   }
 
-  // Detect if request is from Googlebot
-  const userAgent = request.headers.get("user-agent") || "";
-  const isGooglebot = /Googlebot|Googlebot-Mobile|Googlebot-Image/i.test(userAgent);
-  if (isGooglebot) {
-    return NextResponse.next(); // Allow Googlebot to see the original URL
-  }
-
-  // Check if a locale is already in the URL
+  // Check if a locale is specified in the URL
   const localeInPath = SUPPORTED_LOCALES.find((locale) => pathname.startsWith(`/${locale}`));
   if (localeInPath) {
+    // Store the locale in cookies
     const response = NextResponse.next();
     response.cookies.set("locale", localeInPath, { path: "/" });
+
     return response;
   }
 
-  // Check for locale in cookies
+  // Check for the locale in cookies
   const localeFromCookie = request.cookies.get("locale");
+
   if (localeFromCookie && SUPPORTED_LOCALES.includes(localeFromCookie.value)) {
-    return NextResponse.redirect(
-      new URL(`/${localeFromCookie.value}${pathname}`, request.url),
-      302
-    );
+    // Redirect to the saved locale
+    return NextResponse.redirect(new URL(`/${localeFromCookie.value}${pathname}`, request.url));
   }
 
-  // Redirect only for normal users, not Googlebot
+  // If no locale is specified or it is not supported, redirect to the default locale
   return NextResponse.redirect(new URL(`/${DEFAULT_LOCALE}${pathname}`, request.url));
 }
