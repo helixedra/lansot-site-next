0a1760ba816930b061fa805064092558084215f3
helixedra <greencastle08@gmail.com>
2025-03-15 23:40:14 +0100

    gtag

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,34 @@
 import { cookies } from "next/headers";
+import Script from "next/script";
 
 export default async function RootLayout({ children }) {
   const lang = (await cookies()).get("locale");
 
   return (
     <html lang={lang.value}>
-      <body>{children}</body>
-      
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M7SMSVT');`}
+        </Script>
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M7SMSVT"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
+        {children}
+      </body>
     </html>
   );
 }
